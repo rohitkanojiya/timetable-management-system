4fa1ed647c02be1e0fd3b52a80e51ad9c3241c1a
Rohit Kanojiya <34964993+rohitkanojiya@users.noreply.github.com>
2021-08-13 13:09:08 +0530

    Add files via upload

---
+++ inde.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Timetable Management System</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index_css.css">
+</head>
+<body>
+<div>
+	<center><h1>TIMETABLE MANAGEMET SYSTEM</h1></center>				
+</div>
+<div class="container">
+    <div class="topnav">
+      <a href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="contactus.html">Contact</a>
+      <a href="about_us.html">About</a>
+    </div>
+  </div>
+<center><h1 class="title"> User Login</h1>
+<div class="btn">
+<a href="admin/admin.php" class="success">Admin</a>
+<a href="Faculty/slot_view.php" class="info">Faculty</a>
+<a href="student/slot_view.php" class="warning">Student</a>
+</div></center>
+</body>
+</html>
